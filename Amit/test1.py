cdce3ff4cc6b641087985cbb8d14456a53ca119f
Amit Charran <amit.charran@gmail.com>
2020-11-22 16:13:12 -0500

    added infor on how to use data frame

--- Amit/test1.py
+++ Amit/test1.py
@@ -2,4 +2,21 @@ import pandas as pd
 
 df = pd.read_csv("/Users/amitcharran/Desktop/weekend.csv")
 
-print(df)
+# print(df)
+
+df.set_index("0-1")
+
+print(df.loc[0:6,"0-1":"5-6"])
+
+# gets an entire column
+print(df.loc[:, "5-6"])
+
+# also get entire column
+print(df["5-6"])
+
+
+# get row
+print(df.loc[1,:])
+
+# get row
+print(df.iloc[0, 4])
